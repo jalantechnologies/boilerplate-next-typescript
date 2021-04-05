0c8fff91b8610ab6cbc92f8837a5a33be5edee84
agentraghav <raghvendrarathore2@gmail.com>
2021-04-05 18:17:56 +0530

    App component

---
+++ components/app/app.component.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import { Home } from '@components';
+
+import './app.styles.css';
+
+const AppComponent = (): JSX.Element => {
+  return <Home title='Hello World' />;
+};
+
+export default App;
