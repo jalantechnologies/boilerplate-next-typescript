a11841bb404904e0a17b4fcb5ea89d0235b7a4d1
agentraghav <raghvendrarathore2@gmail.com>
2021-04-10 20:38:16 +0530

    added image relative path

--- components/app/app.component.tsx
+++ components/app/app.component.tsx
@@ -2,13 +2,11 @@ import * as React from 'react';
 import { HomeComponent } from '@components';
 import Image from 'next/image';
 
-
-
 const AppComponent = (): JSX.Element => {
   return (
     <div>
       <Image
-        src={`${process.env.baseUrl}/assets/images/logo.png`}
+        src={require('@assets/images/logo.png')}
         alt='logo'
         width={200}
         height={200}
--- next.config.js
+++ next.config.js
@@ -1,14 +1,2 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-const baseUrl = '';
-
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: baseUrl,
-  env: {
-    baseUrl: baseUrl,
-  },
-});
+const withImages = require('next-images');
+module.exports = withImages();
