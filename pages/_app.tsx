1a1c04e133386f58e360d3e47621df136cc1df55
agentraghav <raghvendrarathore2@gmail.com>
2021-04-12 23:21:52 +0530

    added sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import CONFIG from '@config';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+if (CONFIG.sentryDSN) {
+  Sentry.init({
+    dsn: CONFIG.sentryDSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
