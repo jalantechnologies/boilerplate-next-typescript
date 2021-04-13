87535e791bc991636defa2bd47e3e5b4490a986c
agentraghav <raghvendrarathore2@gmail.com>
2021-04-13 13:33:18 +0530

    linting support added

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/'
+  ],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.ts',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts',
-  },
-};
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.ts'
+  }
+}
