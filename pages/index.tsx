049b14bc61f8f6e75865e71c5246fd9eb03c9f52
agentraghav <35917821+agentraghav@users.noreply.github.com>
2021-04-14 15:44:59 +0530

    IN-49 Added automated testing and linting support (#7)
    * added jest for testing
    
    * automated testing added using jest
    
    * linting support added
    
    * linting support added
    
    * linting support
    
    * updates
    
    * updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
@@ -6,7 +7,11 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+
+        <link
+          href="/favicon.ico"
+          rel="icon"
+        />
       </Head>
 
       <main className={styles.main}>
@@ -16,33 +21,44 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
+
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a
+            className={styles.card}
+            href="https://nextjs.org/docs"
+          >
             <h3>Documentation &rarr;</h3>
+
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a
+            className={styles.card}
+            href="https://nextjs.org/learn"
+          >
             <h3>Learn &rarr;</h3>
+
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
+            href="https://github.com/vercel/next.js/tree/master/examples"
           >
             <h3>Examples &rarr;</h3>
+
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           >
             <h3>Deploy &rarr;</h3>
+
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
@@ -53,11 +69,16 @@ export default function Home() {
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+
+          <img
+            alt="Vercel Logo"
+            className={styles.logo}
+            src="/vercel.svg"
+          />
         </a>
       </footer>
     </div>
