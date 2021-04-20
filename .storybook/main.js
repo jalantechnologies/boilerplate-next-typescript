6293d2f1eae56e844a703735caf2fccc89bdde8f
agentraghav <raghvendrarathore2@gmail.com>
2021-04-20 14:14:38 +0530

    resolve updates

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,8 +13,8 @@ module.exports = {
     return config;
   },
   stories: [
-    '../components/**/*.stories.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/stories.mdx',
+    '../components/**/stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
